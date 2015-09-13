0c31da0de4327d58799d2f27db1013afe36f9a0f
unknown <jo2014@users.noreply.github.com>
2015-09-13 23:01:48 +0200

    update

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ with(data, {
   lines(Sub_metering_2~DateTime,col='Red')
   lines(Sub_metering_3~DateTime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+legend("topright", col=c("black", "red", "blue"), lty=c(1,1,1), 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #Save as png file
--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ with(data, {
   plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~DateTime,col='Red')
   lines(Sub_metering_3~DateTime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+  legend("topright", col=c("black", "red", "blue"), lty=c(1,1,1), bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~DateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="datetime")
 })
